48e24a82b135d4fde8c3631bb58a9a523c02f0e5
Henrique Balmant <henrique.balmant@gmail.com>
2021-05-27 05:05:06 -0300

    Add show article

--- src/controllers/ArticlesController.js
+++ src/controllers/ArticlesController.js
@@ -1,7 +1,29 @@
 import Prisma from '@prisma/client';
 const { PrismaClient } = Prisma;
 
+const prisma = new PrismaClient();
+
 class ArticlesController {
+	// Show article
+	async show(request, response) {
+		const { id } = request.params;
+
+		const article = await prisma.article.findUnique({
+			where: {
+				id: parseInt(id),
+			},
+		});
+
+		if (!article) {
+			return response.status(404).json({ message: 'Article not found.' });
+		}
+
+    return response.json({
+      ...article
+    })
+	}
+
+	// Create article
 	async create(request, response) {
 		const { title, published, author } = request.body;
 
@@ -10,8 +32,6 @@ class ArticlesController {
 			published,
 			author,
 		};
-
-		const prisma = new PrismaClient();
 
 		const article = await prisma.article.create({
 			data,
--- src/routes.js
+++ src/routes.js
@@ -9,5 +9,7 @@ const articlesController = new ArticlesController();
 // Article Routes
 // Create article
 routes.post('/articles', articlesController.create);
+// Show article
+routes.get('/articles/:id', articlesController.show);
 
 export default routes;
